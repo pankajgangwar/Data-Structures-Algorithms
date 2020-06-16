a219fd905f99e549f0bca19686d35b7584fef563
Pankaj Kumar <pankaj.gat@gmail.com>
2020-06-16 08:47:55 +0530

    LC Weekly Contest 193

--- app/src/main/java/com/mission/google/leetcode/JuneW2.java
+++ app/src/main/java/com/mission/google/leetcode/JuneW2.java
@@ -10,6 +10,7 @@ import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.PriorityQueue;
 import java.util.Queue;
 import java.util.Set;
 import java.util.Stack;
@@ -37,14 +38,20 @@ class JuneW2 {
         };
         //obj.findLongestChain(pairs);
         //obj.printLIS(new int[]{10,9,2,5,3,7,101,18});
-        obj.largestDivisibleSubset(new int[]{2,8,18,19,20,4,16});
+        //obj.largestDivisibleSubset(new int[]{2,8,18,19,20,4,16});
+        int[][] times = new int[][]{{1,2,1},{2,3,2},{1,3,2}};
+        obj.networkDelayTime(times, 3, 1);
+        //System.out.println("res = " + res);
+        //obj.findLeastNumOfUniqueInts(new int[]{4,3,1,1,3,3,2}, 3);
+        /*TreeAncestor treeAncestor = new TreeAncestor(7, new int[]{-1,0,0,1,1,2,2});
+        System.out.println("treeAncestor.getKthAncestor(3,1) = " + treeAncestor.getKthAncestor(3,1));
+        System.out.println("treeAncestor.getKthAncestor(5,2) = " + treeAncestor.getKthAncestor(5,2));
+        System.out.println("treeAncestor.getKthAncestor(6,3) = " + treeAncestor.getKthAncestor(6,3));*/
     }
 
-    /* https://leetcode.com/problems/maximum-xor-of-two-numbers-in-an-array/ */
+    /* https://leetcode.com/problems/maximum-average-subtree/ */
 
-    /* LC : 743
-    https://leetcode.com/problems/network-delay-time/
-    */
+    /* https://leetcode.com/problems/maximum-xor-of-two-numbers-in-an-array/ */
 
     /*
     LC : 920
@@ -54,19 +61,320 @@ class JuneW2 {
         return 0;
     }
 
+    /* https://leetcode.com/problems/minimum-number-of-days-to-make-m-bouquets/ */
+    public int minDays(int[] bloomDay, int m, int k) {
+        return 0;
+    }
+
+    /*
+    LC : 669
+    https://leetcode.com/problems/trim-a-binary-search-tree/ */
+    public TreeNode trimBST(TreeNode root, int l, int r) {
+        return trim(root, l , r);
+    }
+
+    public TreeNode trim1(TreeNode root, int l, int r){
+        if(root == null) return null;
+        if(root.val == r){
+            root.right = null;
+            root.left = trim1(root.left, l, r);
+            return root;
+        }
+        if(root.val == l){
+            root.left = null;
+            root.right = trim1(root.right, l, r);
+            return root;
+        }
+        if(root.val < l && root.val < r){
+            return trim1(root.right, l, r);
+        }
+        if(root.val > r && root.val > l){
+            return trim1(root.left, l, r);
+        }
+        root.left = trim1(root.left, l, r);
+        root.right = trim1(root.right, l, r);
+        return root;
+    }
+
+    public TreeNode trim(TreeNode root, int l, int r) {
+        if (root == null) return root;
+        if (root.val > r) return trim(root.left, l, r);
+        if (root.val < l) return trim(root.right, l, r);
+
+        root.left = trim(root.left, l, r);
+        root.right = trim(root.right, l, r);
+        return root;
+    }
+
+    /* https://cp-algorithms.com/graph/lca_binary_lifting.html
+    *  https://leetcode.com/problems/kth-ancestor-of-a-tree-node/
+    *  Binary lifting
+    */
+    static class TreeAncestor {
+        Map<Integer, List<Integer>> children = new HashMap<>();
+        Integer[][] memo;
+        public TreeAncestor(int n, int[] parents) {
+            memo = new Integer[n][30];//30 is max depth
+            for (int i = 0; i < n; i++) {
+                int currNode = i;
+                int parent = parents[i];
+                children.computeIfAbsent(parent, value ->  new ArrayList<Integer>()).add(currNode);
+                if(i > 0)memo[currNode][0] = parent;
+            }
+            dfs(0);
+        }
+
+        private void dfs(int currNode) {
+            for (int i = 1; memo[currNode][i-1] != null ; i++) {
+                int parent = memo[currNode][i-1];
+                memo[currNode][i] = memo[parent][i-1];
+            }
+            for(int child : children.getOrDefault(currNode, new ArrayList<>())){
+                dfs(child);
+            }
+        }
+
+        public int getKthAncestor(int node, int k) {
+            int currPow = 0;
+            while (k > 0 ){
+                if(k % 2 == 1){
+                    if(memo[node][currPow] == null) return -1;
+                    node = memo[node][currPow];
+                }
+                currPow++;
+                k = k / 2;
+            }
+            return node;
+        }
+    }
+
+    /*
+    LC : 5454
+    https://leetcode.com/problems/least-number-of-unique-integers-after-k-removals/
+    */
+    public int findLeastNumOfUniqueInts(int[] arr, int k) {
+        HashMap<Integer, Integer> freqMap = new HashMap<>();
+
+        for (int i = 0; i < arr.length; i++) {
+            freqMap.put(arr[i], freqMap.getOrDefault(arr[i], 0) + 1);
+        }
+        PriorityQueue<Map.Entry<Integer, Integer>> pq = new PriorityQueue<>((a,b) -> a.getValue() - b.getValue());
+        for (Map.Entry<Integer, Integer> entry : freqMap.entrySet()){
+            pq.offer(entry);
+        }
+        while (k > 0){
+            Map.Entry<Integer, Integer> entry = pq.poll();
+            if(entry.getValue() > k){
+                entry.setValue(entry.getValue() - k );
+                k = entry.getValue() - k;
+                pq.offer(entry);
+            }else{
+                k = k - entry.getValue();
+            }
+        }
+        return pq.size();
+    }
+
+    /*
+     LC : 5453
+     https://leetcode.com/problems/running-sum-of-1d-array/
+    */
+    public int[] runningSum(int[] nums) {
+        int sum = 0;
+        int[] res = new int[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+            res[i] = sum;
+        }
+        return res;
+    }
+
+    /*
+    LC : 1477
+    https://leetcode.com/problems/find-two-non-overlapping-sub-arrays-each-with-target-sum/ */
+    public int minSumOfLengths(int[] arr, int target) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        map.put(0, -1);
+        int n = arr.length;
+        int[] minLengthAt = new int[n];
+        Arrays.fill(minLengthAt, Integer.MAX_VALUE);
+        int sum = 0;
+        int res = Integer.MAX_VALUE;
+        int best = Integer.MAX_VALUE;
+        for (int i = 0; i < arr.length; i++) {
+            sum += arr[i];
+            Integer prevIdx = map.get(sum - target);
+            if(prevIdx != null){
+                if(prevIdx > -1 && minLengthAt[prevIdx] != Integer.MAX_VALUE) {
+                    res = Math.min(res, i - prevIdx + minLengthAt[prevIdx]);//min len of first sub-array + min len of curr sub-array
+                }
+                best = Math.min(best, i - prevIdx);// min len of curr sub-array
+            }
+            minLengthAt[i] = best;//min len of curr-sub-array at pos i
+            map.put(sum, i);
+        }
+        return res == Integer.MAX_VALUE ? -1 : res;
+    }
+    /*
+     LC : 1476
+     https://leetcode.com/problems/subrectangle-queries/ */
+    class SubrectangleQueries {
+        int[][] matrix;
+        public SubrectangleQueries(int[][] rectangle) {
+            matrix = rectangle;
+        }
+
+        public void updateSubrectangle(int row1, int col1, int row2, int col2, int newValue) {
+            for (int i = row1; i <= row2 ; i++) {
+                for (int j = col1; j <= col2 ; j++) {
+                    matrix[i][j] = newValue;
+                }
+            }
+        }
+        public int getValue(int row, int col) {
+            return matrix[row][col];
+        }
+    }
+
+    /*
+    LC : 1475
+    https://leetcode.com/problems/final-prices-with-a-special-discount-in-a-shop/ */
+    public int[] finalPrices(int[] prices) {
+        int[] res = new int[prices.length];
+        for (int i = 0; i < prices.length; i++) {
+            boolean flag = false;
+            for (int j = i + 1; j < prices.length ; j++) {
+                if(prices[j] <= prices[i] ){
+                    res[i] = prices[i] - prices[j];
+                    flag = true;
+                    break;
+                }
+            }
+            if(!flag){
+                res[i] = prices[i];
+            }
+        }
+        return res;
+    }
+
+    /* LC : 743
+    https://leetcode.com/problems/network-delay-time/
+    */
+    public int networkDelayTime(int[][] times, int N, int K) {
+        int maxTime = findMaxTimeDijikstra(times, N, K);
+        System.out.println("maxTime = " + maxTime);
+        return maxTime ;
+    }
+
+    // https://cp-algorithms.com/graph/bellman_ford.html
+    public int findMaxBellmanFord(int[][] time, int N, int K){
+        // Bellman-Ford O(N*E)
+        // Runs N-1 times for relaxation
+        double[] disTo = new double[N];
+        Arrays.fill(disTo, Double.POSITIVE_INFINITY);
+        disTo[K - 1] = 0;
+        for (int i = 1; i < N; i++) {
+            for (int[]edges : time){
+                int u = edges[0] - 1, v = edges[1] - 1, w = edges[2];
+                if(disTo[u] + w < disTo[v]){
+                    disTo[v] = disTo[u] + w;
+                }
+            }
+        }
+        double res = Double.MIN_VALUE;
+        for (double val : disTo){
+            res = Math.max(res, val);
+        }
+        return res == Double.POSITIVE_INFINITY ? -1 : (int)res;
+    }
+
+    public int findMaxFloydWarshall(int[][] times, int N, int K){
+        // Floyd–Warshall algorithm O(n^3)
+        double[][] distTo = new double[N][N];
+        for (int i = 0; i < N; i++) {
+            Arrays.fill(distTo[i], Double.POSITIVE_INFINITY);
+        }
+        for (int i = 0; i < N; i++) {
+            distTo[i][i] = 0;
+        }
+        for (int[] edge : times) {
+            int src = edge[0];
+            int dst = edge[1];
+            distTo[src - 1][dst - 1] = edge[2];
+        }
+
+        for (int k = 0; k < N; k++) {
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < N; j++) {
+                    if(distTo[i][j] > distTo[i][k] + distTo[k][j]){
+                        distTo[i][j] = distTo[i][k] + distTo[k][j];
+                    }
+                }
+            }
+        }
+        double max = Double.MIN_VALUE;
+        for (int i = 0; i < N; i++) {
+            if(distTo[K-1][i] == Double.POSITIVE_INFINITY) return -1;
+            max = Math.max(max, distTo[K - 1][i]);
+        }
+        return (int)max;
+    }
+
+    public int findMaxTimeDijikstra(int[][] times, int N, int K){
+        LinkedList<int[]>[] graph = new LinkedList[N];
+        for (int i = 0; i < N; i++) {
+            graph[i] = new LinkedList<>();
+        }
+        for (int i = 0; i < times.length; i++) {
+            int src = times[i][0] - 1;
+            int dst = times[i][1] - 1;
+            int time = times[i][2];
+            graph[src].add(new int[]{dst, time});
+        }
+        boolean[] visited = new boolean[N];
+        Arrays.fill(visited, false);
+
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b) -> a[1] - b[1]);
+        pq.offer(new int[]{K - 1, 0});
+        int totalTime = 0;
+        while (!pq.isEmpty()){
+            int[] cur = pq.poll();
+            int currSrc = cur[0];
+            int currtime = cur[1];
+            if(visited[currSrc]) continue;
+            visited[currSrc] = true;
+            totalTime = Math.max(totalTime, currtime);
+            N--;
+            for (int i = 0; i < graph[currSrc].size(); i++) {
+                int[] next = graph[currSrc].get(i);
+                int nextSrc = next[0];
+                int time = next[1];
+                time = currtime + time;
+                pq.offer(new int[]{nextSrc, time});
+            }
+        }
+        for (int i = 0; i < visited.length; i++) {
+            if(!visited[i]){
+                totalTime = 0;
+                break;
+            }
+        }
+        return N == 0 ? totalTime : -1;
+    }
+
     /*
     LC : 510
     https://leetcode.com/problems/inorder-successor-in-bst-ii/
     */
-    class InorderNode {
+    class NodeX {
         public int val;
-        public InorderNode left;
-        public InorderNode right;
-        public InorderNode parent;
+        public NodeX left;
+        public NodeX right;
+        public NodeX parent;
     };
-    public InorderNode inorderSuccessor(InorderNode node) {
+    public NodeX inorderSuccessor(NodeX node) {
         if(node.right == null){
-            InorderNode parent = node.parent;
+            NodeX parent = node.parent;
             while (parent != null && parent.val < node.val){
                 parent = parent.parent;
             }

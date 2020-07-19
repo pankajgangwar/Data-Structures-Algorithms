e76fe275efc5f1602430e7134bfc91757d5280ae
Pankaj Kumar <pankaj.gat@gmail.com>
2020-07-19 12:04:55 +0530

    Fenwick Tree Data structure

--- app/src/main/java/com/mission/google/datastructures/FenwickTree/FenwickTreeTest.java
+++ app/src/main/java/com/mission/google/datastructures/FenwickTree/FenwickTreeTest.java
@@ -1,4 +1,26 @@
 package com.mission.google.datastructures.FenwickTree;
 
 class FenwickTreeTest {
-   
+    public static void main(String[] args) {
+        int n = 10;
+        long[] values = new long[]{0L, 1L, 2L, 3L, 4L, 5L, 6L, 7L};
+
+        RangeQueryPointUpdate rangeQueryPointUpdate = new RangeQueryPointUpdate(values);
+        long sum = rangeQueryPointUpdate.sum(1, 4);
+        System.out.println("sum = " + sum);
+
+        rangeQueryPointUpdate.add(4, 1L);
+        long sum1 = rangeQueryPointUpdate.sum(1, 1);
+        System.out.println("sum = " + sum1);
+
+        rangeQueryPointUpdate.set(1, 4L);
+        long sum2 = rangeQueryPointUpdate.sum(1, 1);
+        System.out.println("sum = " + sum2);
+
+        RangeUpdatePointQuery pointQuery = new RangeUpdatePointQuery(values);
+        pointQuery.updateRange(1, 2, 1L);
+        long sum3 = pointQuery.get(2);
+        System.out.println("sum3 = " + sum3);
+
+    }
+}
--- app/src/main/java/com/mission/google/datastructures/FenwickTree/RangeQueryPointUpdate.java
+++ app/src/main/java/com/mission/google/datastructures/FenwickTree/RangeQueryPointUpdate.java
@@ -1,5 +1,61 @@
 package com.mission.google.datastructures.FenwickTree;
 
+import java.util.Arrays;
+
 class RangeQueryPointUpdate {
-    
+    final int N;
+    private long[] tree;
+
+    public RangeQueryPointUpdate(int sz){
+        tree = new long[(N  = sz + 1)];
+    }
+
+    public RangeQueryPointUpdate(long[] values) {
+        if(values == null) throw new IllegalArgumentException("Values array cannot be null !!");
+        N = values.length;
+        values[0] = 0L;
+
+        tree = values.clone();
+
+        for (int i = 0; i < N; i++) {
+            int parent = i + lsb(i);
+            if(parent < N ) tree[parent] += tree[i];
+        }
+    }
+
+    private int lsb(int i) {
+        return i & -i;
+        //return Integer.lowestOneBit(i);
+    }
+
+    private long prefixSum(int i) {
+        long sum = 0L;
+        while (i != 0){
+            sum += tree[i];
+            i &= ~lsb(i); // Equivalent to , i -= lsb(i);
+        }
+        return sum;
+    }
+
+    public long sum(int left, int right) {
+        if(right < left) throw new IllegalArgumentException("Make sure right >= left");
+        return prefixSum(right) - prefixSum(left - 1);
+    }
+
+    public void add(int i, long v){
+        while (i < N){
+            tree[i] += v;
+            i += lsb(i);
+        }
+    }
+
+    public void set(int i, long v){
+        long val = sum(i, i);
+        add(i, v - val);
+    }
+
+    @Override
+    public String toString() {
+        return Arrays.toString(tree);
+    }
 }

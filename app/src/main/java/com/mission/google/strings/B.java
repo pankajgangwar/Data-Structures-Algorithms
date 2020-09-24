20102dbdbc81facccc41a489f2eadfcdb3c6e96d
Pankaj Kumar <pankaj.gat@gmail.com>
2020-09-24 08:39:29 +0530

    LC Problem : 1592

--- app/src/main/java/com/mission/google/strings/B.java
+++ app/src/main/java/com/mission/google/strings/B.java
@@ -1,9 +1,75 @@
 package com.mission.google.strings;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by Pankaj Kumar on 06/September/2020
  */
 class B {
+
+    /* 1592. Rearrange Spaces Between Words
+     * https://leetcode.com/problems/rearrange-spaces-between-words/
+     * */
+    public String reorderSpaces(String text) {
+        List<String> words = new ArrayList<>();
+        StringBuilder builder = new StringBuilder();
+        int spaces = 0;
+        int n = text.length();
+        for(int i = 0; i < text.length(); i++){
+            if(text.charAt(i) == ' '){
+                while (i < n && text.charAt(i) == ' '){
+                    spaces++;
+                    i++;
+                }
+                i = i - 1;
+                if(builder.length() > 0){
+                    words.add(builder.toString());
+                }
+                builder = new StringBuilder();
+            }else{
+                builder.append(text.charAt(i));
+            }
+        }
+        if(builder.length() > 0){
+            words.add(builder.toString());
+        }
+
+        int req = 0;
+        if(spaces < words.size()){
+            return text;
+        }else if(spaces == words.size()){
+            req = 1;
+        }else{
+            if(words.size() - 1 == 0 ){
+                req = spaces;
+            }else{
+                req = spaces / (words.size() - 1);
+            }
+        }
+        int currLen = 0;
+        int i = 0;
+        StringBuilder out = new StringBuilder();
+        while (currLen < n && i < words.size()){
+            String word = words.get(i++);
+            currLen += word.length();
+            out.append(word);
+            if(currLen == n){
+                break;
+            }
+            int temp = req;
+            while (temp > 0 && currLen < n){
+                out.append(" ");
+                temp--;
+                currLen++;
+            }
+        }
+        while (currLen < n) {
+            out.append(" ");
+            currLen++;
+        }
+        return out.toString();
+    }
 
     /* 65. Valid Number
     * https://leetcode.com/problems/valid-number/

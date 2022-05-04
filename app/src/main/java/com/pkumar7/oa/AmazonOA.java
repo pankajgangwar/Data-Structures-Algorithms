867a8ea39a685e09539dc0960cfdc08da9a3e43c
pankajgangwar <pankaj.gat@gmail.com>
2022-05-04 12:16:00 +0530

    Added new amazon OA problem

--- app/src/main/java/com/pkumar7/oa/AmazonOA.java
+++ app/src/main/java/com/pkumar7/oa/AmazonOA.java
@@ -2,6 +2,7 @@ package com.pkumar7.oa;
 
 
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.LinkedList;
 
 public class AmazonOA {
@@ -10,6 +11,54 @@ public class AmazonOA {
         AmazonOA current = new AmazonOA();
         current.maxDeviation("aacaaca");
     }
+
+    /*
+    * https://leetcode.com/discuss/interview-question/1802061/Amazon-SDE2-OA
+    * Q2. Given an array ranks of ranks of students in a school.
+    * All students need to be split into groups k. Find the total 'imbalance' of all groups.
+    * An imabalance of a group can be found as :
+
+    Sorting each group in the order of their ranks.
+    A group contributes to imbalance if any 2 students in the sorted array have a rank difference of more than 1.
+    Find the total sum of imbalance of all such groups.
+    * */
+    private static int totalImbalance(int[] rank) {
+        int rankSize = rank.length;
+        int[] ldp = new int[rankSize];
+        int[] rdp = new int[rankSize];
+        Arrays.fill(ldp, -1);
+        Arrays.fill(rdp, rankSize);
+        HashMap<Integer, Integer> check = new HashMap<Integer, Integer>();
+        for(int i=0; i < rankSize; i++)
+        {
+            if(check.containsKey(rank[i]+1))
+                ldp[i] = check.get(rank[i]+1);
+            check.put(rank[i], i);
+        }
+        check = new HashMap<Integer, Integer>();
+        for(int i=rankSize-1; i >=0; i--)
+        {
+            if(check.containsKey(rank[i]+1))
+                rdp[i] = check.get(rank[i]+1);
+            check.put(rank[i], i);
+        }
+        int ans = 0;
+        for(int i=0; i < rankSize; i++)
+        {
+            int start = i-ldp[i];
+            int end = rdp[i]-i;
+            int j =i, k=i;
+            while(j > ldp[i] && rank[j] <= rank[i])
+                j--;
+            while(k < rdp[i] && rank[k] <= rank[i])
+                k++;
+            int max = start*end;
+            int min = (i-j)*(k-i);
+            ans+= (max-min);
+        }
+        return ans;
+    }
+
     /*
     * https://leetcode.com/discuss/interview-question/1742621/Amazon-or-OA-or-Max-deviation-among-all-substrings
     * https://www.pepcoding.com/resources/data-structures-and-algorithms-in-java-levelup/dynamic-programming/maximum_sum_subarray_with_at_least_k_elements/topic
